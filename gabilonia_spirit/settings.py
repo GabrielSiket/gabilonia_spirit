85e58ba9eed8f8a352a3a7573e00e695b964a52e
Gabriel S <gabriel.siket@gmail.com>
2022-11-30 20:19:30 +0000

    add checkout views and templates

--- gabilonia_spirit/settings.py
+++ gabilonia_spirit/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'gabilonia_spirit.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -64,6 +68,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]

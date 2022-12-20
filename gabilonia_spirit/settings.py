f6eca59cfe9fb2deca7a703d04a5c74f0efc1481
Gabriel S <gabriel.siket@gmail.com>
2022-12-20 13:26:04 +0000

    remove secret key and set debug

--- gabilonia_spirit/settings.py
+++ gabilonia_spirit/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hjf3w3llrqb0@2n*2qre%*b3uhgy*wjhcdk14e(4@c8$_ln9mt'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gabilonia-spirit.herokuapp.com', 'localhost']
 

02f1cb9ee24e957be29465e8aa92316bbe62491a
Gabriel S <gabriel.siket@gmail.com>
2022-12-20 12:14:00 +0000

    deployment part2

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gabilonia_spirit.wsgi:application
--- gabilonia_spirit/settings.py
+++ gabilonia_spirit/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -14,7 +15,7 @@ SECRET_KEY = 'django-insecure-hjf3w3llrqb0@2n*2qre%*b3uhgy*wjhcdk14e(4@c8$_ln9mt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gabilonia-spirit.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -102,15 +103,17 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'gabilonia_spirit.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
